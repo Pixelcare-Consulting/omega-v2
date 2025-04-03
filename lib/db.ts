59b2332efa596f8cf5900000b6ab12e18e762e19
GMJSilmaro <dzheysilmaro@gmail.com>
2025-04-03 16:32:03 +0800

    Update README.md to include project description, setup instructions, and resources for Next.js

---
+++ lib/db.ts
@@ -0,0 +1,21 @@
+// @ts-expect-error - Prisma Client is not available during build time but will be available at runtime
+import { PrismaClient } from '@prisma/client'
+import { global } from 'styled-jsx/css'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient
+    }
+  }
+}
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+})
+
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
