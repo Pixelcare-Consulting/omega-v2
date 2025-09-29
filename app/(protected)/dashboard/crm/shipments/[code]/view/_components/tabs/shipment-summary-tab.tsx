d276b75ad9d3d176f9cb02bea8419c2b94c5dbd6
Mond <reymondgomera24@gmail.com>
2025-09-29 17:00:01 +0800

    fix: fix client directive typo and modify read only field component

--- app/(protected)/dashboard/crm/shipments/[code]/view/_components/tabs/shipment-summary-tab.tsx
+++ app/(protected)/dashboard/crm/shipments/[code]/view/_components/tabs/shipment-summary-tab.tsx
@@ -1,4 +1,4 @@
-"usse client"
+"use client"
 
 import { useEffect } from "react"
 import { format, isValid } from "date-fns"
--- components/read-only-field.tsx
+++ components/read-only-field.tsx
@@ -1,17 +1,28 @@
 import { cn } from "@/lib/utils"
+import { Icons } from "./icons"
 
 type ReadOnlyFieldProps = {
   className?: string
   title: string
   value: React.ReactNode
+  isLoading?: boolean
   description?: string
 }
 
-export default function ReadOnlyField({ className, title, value, description }: ReadOnlyFieldProps) {
+export default function ReadOnlyField({ className, title, value, isLoading, description }: ReadOnlyFieldProps) {
   return (
     <div className={cn("flex flex-col justify-start", className)}>
       <h2 className='mb-1 text-sm text-muted-foreground'>{title}:</h2>
-      <span className='inline-block h-full rounded bg-muted/80 p-3 text-sm font-semibold'>{value}</span>
+      <span className='inline-block h-full rounded bg-muted/80 p-3 text-sm font-semibold'>
+        {!isLoading && value}
+
+        {isLoading && (
+          <div className='flex items-center justify-between'>
+            <span className='text-muted-foreground'>Loading value...</span>
+            <Icons.spinner className='size-4 animate-spin' />
+          </div>
+        )}
+      </span>
       <span className='mt-1 text-xs text-muted-foreground'>{description}</span>
     </div>
   )
